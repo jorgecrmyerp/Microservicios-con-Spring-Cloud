a435c44fedf4ea04f1b67b64b6b8fc5a7c5125df
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-11-07 11:29:40 +0100

    lu.,  7 de nov. de 2022 10:59:342022-11-07 PROMETHEUS++grafana

---
+++ MIOS/grafana/public/app/extensions/reports/constants.ts
@@ -0,0 +1,4 @@
+export const defaultReportLogo = '/public/img/grafana_icon.svg';
+export const defaultEmailLogo = 'https://grafana.com/static/assets/img/grafana_logo_lockup_ltbg.png';
+
+export const BASE_URL = '/reports';
